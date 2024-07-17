4bda52a6314c03179466cff036ec04b3a4bca854
Ciclonix <72094161+Ciclonix@users.noreply.github.com>
2024-07-17 16:00:03 +0200

    edit index.html, script.js, style.css

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
 const container = document.querySelector(".container");
-const button = document.querySelector("#change_squares")
-let side_len = 4, r = 0, g = 0, b = 0;
+const slider = document.querySelector(".slider");
+const sides = document.querySelector(".sides")
+const random_color = document.querySelector(".random_color");
+const color_pick = document.querySelector("#color_pick");
+const shading = document.querySelector(".shading")
+const eraser = document.querySelector(".eraser");
+const see_sq = document.querySelector(".see_squares");
+let random = false, shade = false, draw = false, erase = false, side_len = 16;
+
+
+function randomColor() {
+    let r, g, b;
+    r = Math.floor(Math.random() * 256);
+    g = Math.floor(Math.random() * 256);
+    b = Math.floor(Math.random() * 256);
+    return Array(r, g, b);
+}
 
 
 function updateLenghts() {
@@ -23,23 +38,26 @@ function createGrid() {
             div_square = document.createElement("div");
             div_square.classList.add("square");
             div_row.appendChild(div_square);
-
-            div_square.addEventListener("mouseover", (event) => {
-                //let opacity = 0;
-                event.target.classList.add("green");
-                //opacity = parseFloat(event.target.style.opacity) + 0.1;
-                //event.target.style.opacity = "" + opacity;
-            })
         }
 
         container.appendChild(div_row);
-        updateLenghts();
     }
+
+    updateLenghts();
 }
 
 
-button.addEventListener("click", () => {
-    side_len = prompt("Set the new number of squares per side");
+// --- Creates first grid (16x16) ---
+
+
+createGrid();
+
+
+// --- Events ---
+
+
+slider.addEventListener("mouseup", () => {
+    side_len = slider.value;
     if (side_len > 100) {
         side_len = 100;
     } else if (side_len < 1) {
@@ -50,4 +68,58 @@ button.addEventListener("click", () => {
 })
 
 
-createGrid();
+slider.addEventListener("mousemove", () => {
+    sides.innerHTML = `${slider.value} x ${slider.value}`
+})
+
+
+random_color.addEventListener("click", () => {
+    random_color.classList.toggle("green_border");
+    random_color.classList.toggle("red_border");
+    random = random ? false : true;
+})
+
+
+shading.addEventListener("click", () => {
+    shading.classList.toggle("green_border");
+    shading.classList.toggle("red_border");
+    shade = shade ? false : true;
+})
+
+
+eraser.addEventListener("click", () => {
+    eraser.classList.toggle("green_border");
+    eraser.classList.toggle("red_border");
+    erase = erase ? false : true;
+})
+
+
+container.addEventListener("click", (event) => {
+    draw = draw ? false : true;
+})
+
+
+container.addEventListener("mouseover", (event) => {
+    const target = event.target;
+
+    if (target.classList.contains("square") && draw) {
+        if (!erase) {
+            if (!random) {
+                target.style.backgroundColor = color_pick.value;
+            } else {
+                target.style.backgroundColor = "rgb(" + randomColor().toString() + ")";
+            }
+            
+            if (shade && ((target.style.opacity == 1) || (target.style.opacity == ""))) {
+                target.style.opacity = 0.1;
+            } else if (shade && target.style.opacity < 1) {
+                target.style.opacity = parseFloat(target.style.opacity) + 0.1;
+            } else {
+                target.style.opacity = 1;
+            }
+        } else {
+            target.style.backgroundColor = "";
+            target.style.opacity = 1;
+        }
+    }
+})
